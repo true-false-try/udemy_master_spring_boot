90edddf711a62cb7e1aac1c084d43a7eb98e20a9
yevhenii <yevhen.chekhovskyi@novaposhtaglobal.ua>
2022-12-24 17:06:45 +0100

    [Section 7] - Demo of SpringBoot Autoconfiguration

--- src/main/java/com/spring/exemple_7/Exemple7Application.java
+++ src/main/java/com/spring/exemple_7/Exemple7Application.java
@@ -2,8 +2,9 @@ package com.spring.exemple_7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Exemple7Application {
 
 	public static void main(String[] args) {
