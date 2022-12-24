cf4caa47f2f6cac4ef2bb00e659556ea859ae810
yevhenii <yevhen.chekhovskyi@novaposhtaglobal.ua>
2022-12-24 12:27:59 +0100

    [Section 7] - created spring boot app

---
+++ src/main/java/com/spring/exemple_7/Exemple7Application.java
@@ -0,0 +1,13 @@
+package com.spring.exemple_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exemple7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exemple7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/exemple_7/Exemple7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.exemple_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemple7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
