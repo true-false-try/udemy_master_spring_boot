642f1cfdbc218dc66969068a189b74dbd2822779
true-false-try <6mateo6ch@gmail.com>
2023-02-17 12:28:54 +0200

    las lecture into chapter 8

--- src/main/java/com/spring/exemple_7/config/WebConfig.java
+++ src/main/java/com/spring/exemple_7/config/WebConfig.java
@@ -1,14 +1,16 @@
 package com.spring.exemple_7.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configurable
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/courses").setViewName("courses");
+
     }
 }
--- src/main/java/com/spring/exemple_7/controller/HomeController.java
+++ src/main/java/com/spring/exemple_7/controller/HomeController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-    @RequestMapping( value = {"", "/", "/home"})
+    @RequestMapping( value = {"", "/", "home"})
     public String displayHomePage(Model model) {
         model.addAttribute("username","John Doe");
         return "home.html";
