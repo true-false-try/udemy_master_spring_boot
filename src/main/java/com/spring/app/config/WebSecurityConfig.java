3d99216f8b482142c72ba5a563b58942373686ce
true-false-try <6mateo6ch@gmail.com>
2023-02-24 22:07:18 +0200

    [Section 13]  Configure custom security configurations using Spring Security

--- src/main/java/com/spring/app/config/WebSecurityConfig.java
+++ src/main/java/com/spring/app/config/WebSecurityConfig.java
@@ -1,21 +1,18 @@
 package com.spring.app.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll()
-                .and().formLogin()
-                .and().httpBasic();
+        http.authorizeRequests()
+                .mvcMatchers("/home").authenticated()
+                .and().formLogin().and().httpBasic();
     }
 
 
